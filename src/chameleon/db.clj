f25410a133385801775cb2a84a92c3db354753ce
Michael Dippery <michael@monkey-robot.com>
2014-05-23 01:36:23 -0700

    Use monger.core/connect-via-uri to connect to database

--- src/chameleon/db.clj
+++ src/chameleon/db.clj
@@ -1,18 +1,10 @@
 (ns chameleon.db
   (:require [chameleon.utils :as utils]
-            [monger.core :as mg])
-  (:import [java.net URI]))
+            [monger.core :as mg]))
 
 
 (def db-url (utils/env "CHAMELEON_MONGODB_URL" "mongodb://localhost:27017/chameleon"))
 
-(defn- db-uri []
-  (URI. db-url))
-
-(def db-host     (.getHost (db-uri)))
-(def db-port     (.getPort (db-uri)))
-(def db-database (.substring (.getPath (db-uri)) 1))
-
 (def db
-  (let [conn (mg/connect {:host db-host :port db-port})]
-    (mg/get-db conn db-database)))
+  (let [conn-and-db (mg/connect-via-uri db-url)]
+    (:db conn-and-db)))
--- test/chameleon/db_test.clj
+++ test/chameleon/db_test.clj
@@ -4,12 +4,3 @@
 
 (deftest test-db-url
   (is (= "mongodb://localhost:27017/chameleon" db-url)))
-
-(deftest test-db-host
-  (is (= "localhost" db-host)))
-
-(deftest test-db-port
-  (is (= 27017 db-port)))
-
-(deftest test-db-database
-  (is (= "chameleon" db-database)))
