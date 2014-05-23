a792b0501616eb554c6baa70726185d9c9af76c2
Michael Dippery <michael@monkey-robot.com>
2014-05-22 19:57:56 -0700

    License under 3-Clause BSD

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject chameleon "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject chameleon "1.0.0"
+  :description "Takes samples of Stack Overflow reputation over time"
+  :url "https://github.com/mdippery/chameleon"
+  :license {:name "3-Clause BSD"
+            :url "http://opensource.org/licenses/BSD-3-Clause"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :main ^:skip-aot chameleon.core
   :target-path "target/%s"
