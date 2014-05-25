a101175de166903009d4314f46d4748616a26ea6
Michael Dippery <michael@monkey-robot.com>
2014-05-24 17:39:28 -0700

    Upgrade to clojure 1.6
    Also upgrade dependencies to latest version.

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/mdippery/chameleon"
   :license {:name "3-Clause BSD"
             :url "http://opensource.org/licenses/BSD-3-Clause"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-http "0.6.3"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-http "0.9.1"]
                  [org.clojure/data.json "0.2.4"]
                  [com.novemberain/monger "2.0.0-rc1"]]
   :main ^:skip-aot chameleon.core
