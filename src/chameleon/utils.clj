1ac985c4af6335d8559d5145093d9f25e0b8c10d
Michael Dippery <michael@monkey-robot.com>
2015-04-09 22:44:19 -0700

    Calculate hours since midnight
    This is pulled from a related project, karmanaut. I'm working on
    redesigning the database to coalesce samples by day. (I really need to
    find a way to abstract this stuff out into a common library...but I'll
    save that for later.)

--- src/chameleon/utils.clj
+++ src/chameleon/utils.clj
@@ -1,8 +1,34 @@
 (ns chameleon.utils
-  (:import [java.util Date]))
+  (:import [java.util Calendar
+                      Date
+                      GregorianCalendar
+                      TimeZone]))
 
 (defn env [key default]
   (get (System/getenv) key default))
 
 (defn utcnow []
   (Date.))
+
+(defn midnight [dt]
+  "Returns the given datetime at midnight"
+  (let [tz (TimeZone/getTimeZone "UTC")
+        c (GregorianCalendar. tz)]
+    (doto c
+      (.setTime dt)
+      (.set Calendar/HOUR_OF_DAY 0)
+      (.set Calendar/MINUTE 0)
+      (.set Calendar/SECOND 0)
+      (.set Calendar/MILLISECOND 0))
+    (.getTime c)))
+
+(defn seconds-since-epoch [dt]
+  "Number of seconds elapsed since the Unix epoch"
+  (quot (.getTime dt) 1000))
+
+(defn hours-since-midnight [dt]
+  "Number of hours elapsed since midnight"
+  (let [mn (midnight dt)
+        mnsec (seconds-since-epoch mn)
+        dtsec (seconds-since-epoch dt)]
+    (quot (- dtsec mnsec) (* 60 60))))
